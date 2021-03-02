b4619d68b34e51da3a86d8e73ed6a73abe72845c
Karetnikova Valeriya <valeriya.karetnikova@gmail.com>
2021-03-01 18:43:39 -0700

    day1 finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 // js code to trigger the button goes here
+// Step 1: query (search) the document and select the element to show and hide
+const button = document.querySelector('.hamburger-btn');
+
+button.addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('show-nav');
+});
